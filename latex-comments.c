346121fc2b1ec835bd2c2190b93b4040957648d1
Muser <micsverau@web.de>
2021-02-03 23:30:01 +0100

    experimented some more

--- latex-comments.c
+++ latex-comments.c
@@ -4,9 +4,10 @@
 
 #include <geanyplugin.h>
 
+
 static void item_activate_cb(GtkMenuItem *menuitem, gpointer user_data)
 {
-    dialogs_show_msgbox(GTK_MESSAGE_INFO, "Hello World");
+    dialogs_show_msgbox(GTK_MESSAGE_INFO, _("Hello World"));
 } 
 static gboolean latexcomments_init(GeanyPlugin *plugin, gpointer pdata)
 {
@@ -35,15 +36,14 @@ G_MODULE_EXPORT
 void geany_load_module(GeanyPlugin *plugin)
 {
     /* Step 1: Set metadata */
-    plugin->info->name = "Latex-Comments";
-    plugin->info->description = "Displays latex-comments as graphic";
+    plugin->info->name = _("Latex-Comments");
+    plugin->info->description = _("Displays latex-comments as graphic");
     plugin->info->version = "0.0";
-    plugin->info->author = "Michelle Rausch <michelle.rausch@protonmail.com>";
+    plugin->info->author = _("Michelle Rausch <michelle.rausch@protonmail.com>");
  
     /* Step 2: Set functions */
     plugin->funcs->init = latexcomments_init;
-    plugin->funcs->cleanup = latexcomments
-    _cleanup;
+    plugin->funcs->cleanup = latexcomments_cleanup;
  
     /* Step 3: Register! */
     GEANY_PLUGIN_REGISTER(plugin, 225);
